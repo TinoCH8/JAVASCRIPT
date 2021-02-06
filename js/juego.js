bce999f63d95426c8bb747e53f531eb2452a38aa
TinoCH8 <71781767+TinoCH8@users.noreply.github.com>
2021-02-05 22:46:34 -0700

    JSON en Js

--- js/juego.js
+++ js/juego.js
@@ -1,11 +1,31 @@
 // me aseguro de que se ha cargado toda la pagina
+var indiceRandom = 0;
+var op = []; 
 
 $(document).ready(function(){
+    /*
     $("#login").animate({
         'top':"10%"
-    },300);
+    },300);*/
+    $.getJSON('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json', 
+    function(data){
+        //console.log(data.elements[0]);
+        var cuantos = data.elements.length;
+        indiceRandom = Math.floor(Math.random() * cuantos);
+        $("#elemento").find('h2').text(data.elements[indiceRandom].name);
+        for(var i=0; i<3; i++){
+            var n=Math.floor( Math.random()* cuantos);
+            op.push(n);
+            $("#opciones").find('ul').append('<li><h2>'+data.elements[n].symbol+'</h2></li>');
+        }
+        $("#opciones").find('ul').append('<li><h2>'+data.elements[indiceRandom].symbol+'</h2></li>');
+        op.push(indiceRandom);
+        console.log(op);
+    });
 
     $("#txtNombre").on('keyup', function(){
+        $("#txtNombre").css('border','1px solid white'); 
+        $(".alerta").fadeOut(400);
 
     });
     $("#btnIniciar").click(function(){
@@ -13,8 +33,6 @@ $(document).ready(function(){
            $("#txtNombre").css('border','1px solid red'); 
            $(".alerta").fadeIn(400);
         }else{
-
-        
         $("#login").animate({
             'left':"-500px"
         },300,function(){
